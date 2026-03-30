443f374919953c725395c128f1b7f429ecf5d4bd
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-03-30 02:05:48 +0200

    Add loading screen with pure CSS.

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
- document.addEventListener('DOMContentLoaded', () => { 
+window.addEventListener('load', () => {
+            const el = document.querySelector('.masked-landing');
+            el.style.transition = 'opacity 0.6s ease';
+            el.style.opacity = '0';
+
+            setTimeout(() => {
+                el.style.display = 'none';
+            }, 3000);
+        });
+
+document.addEventListener('DOMContentLoaded', () => { 
     const images = document.querySelectorAll('.md-slides, .md-slidesB, .md-slides2, .md-slidesB2');
 
     const observer = new IntersectionObserver((entries) => {
